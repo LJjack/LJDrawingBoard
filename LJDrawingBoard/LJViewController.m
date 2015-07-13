cb7ba4b7811a698f70b0bd5b468781dbd1d04801
刘俊杰 <945980335@qq.com>
2015-07-13 18:13:58 +0800

    添加使用方法

--- LJDrawingBoard/LJViewController.m
+++ LJDrawingBoard/LJViewController.m
@@ -5,7 +5,15 @@
 //  Created by liujunjie on 15-7-13.
 //  Copyright (c) 2015年 rj. All rights reserved.
 //
-
+/*
+ 使用方法：
+ 1.引进头文件 #import "LJDrawViewController.h"
+ 2.遵守代理 <LJDrawViewControllerDelegate>
+ 3.添加方法
+ 4.使用模态弹出
+ 注意：放回的image因为是加上工具栏是满屏，
+      所以对image处理时因该注意宽高比
+ */
 #import "LJViewController.h"
 #import "LJDrawViewController.h"
 @interface LJViewController ()<LJDrawViewControllerDelegate>
